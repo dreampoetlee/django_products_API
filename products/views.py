40c7c2fffded8bc662f15e6d2a43affb18f16ddd
Jay Lee <dreampoetlee@gmail.com>
2022-04-18 21:09:31 -0400

    Updated the Admin Site for the Products App + Updated the GET Request in the Views File

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import ProductSerializer
+from .models import Product
 
 @api_view(['GET'])
 def products_list(request):
 
+  product = Product.objects.all()
+
+  serializer = ProductSerializer(product, many=True)
 
 
-    return Response('ok')
+
+  return Response(serializer.data)
