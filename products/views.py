e623d76a2feeed99c219c870cde5e328776d6316
Jay Lee <dreampoetlee@gmail.com>
2022-04-19 07:28:38 -0400

    Added the POST Method to the Products App + Added the HTTP Error Codes

--- products/views.py
+++ products/views.py
@@ -1,15 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
 
-  product = Product.objects.all()
+  if request.method == 'GET':
+    product = Product.objects.all()
+    serializer = ProductSerializer(product, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-  serializer = ProductSerializer(product, many=True)
-
-
-
-  return Response(serializer.data)
+  elif request.method == 'POST':
+    serializer = ProductSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)  
