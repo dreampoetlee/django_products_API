bfb039c5889f124f6d7f0d5fee8bb43023f86b29
Jay Lee <dreampoetlee@gmail.com>
2022-04-21 00:28:09 -0400

    Added the Products User Stories to the README file + added the image model to the Products model

--- products/models.py
+++ products/models.py
@@ -1,8 +1,10 @@
+from email.mime import image
 from turtle import title
 from django.db import models
 
 # Create your models here.
 class Product(models.Model):
+  image = models.CharField(max_length=255)
   title = models.CharField(max_length=255)
   description = models.CharField(max_length=255)
   price = models.DecimalField(max_digits=8, decimal_places=2)
